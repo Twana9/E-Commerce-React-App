3e979dcdefa1a9594af67265f4023fa4bbfc93c9
twana koye <twana.00569020@gmail.com>
2024-07-07 23:18:24 +0300

    nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,31 @@
 import { cartIcon, logo } from "../../assets";
+import "./Navbar.css";
 export function Navbar() {
   return (
-    <div className="flex justify-around shadow-black">
-      <div>
+    <div
+      className="flex justify-evently shadow-nav
+     shadow-black p-4"
+    >
+      <div
+        className="flex flex-1  justify-center
+       items-center gap-5"
+      >
         <img src={logo} alt="logo" />
         <p>SHOPPING</p>
       </div>
-      <ul>
+      <ul
+        className="flex flex-1  justify-center
+       items-center gap-[50px]"
+      >
         <li>Shop</li>
         <li>Men</li>
         <li>Women</li>
         <li>Kids</li>
       </ul>
-      <div>
+      <div
+        className="flex flex-1 justify-center
+       items-center gap-5"
+      >
         <button>Login</button>
         <img src={cartIcon} alt="cart" />
       </div>
