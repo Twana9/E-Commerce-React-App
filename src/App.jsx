618b58b22422ec155e876d701824023662f0abbf
twana koye <twana.00569020@gmail.com>
2024-07-14 23:28:29 +0300

    routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./Components/Navbar/Navbar";
+import { LoginSignup, Product, Shop, ShopCategory } from "./Pages";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/men" element={<ShopCategory state="men" />} />
+        <Route path="/women" element={<ShopCategory state="women" />} />
+        <Route path="/kids" element={<ShopCategory state="kids" />} />
+        <Route path="/product" element={<Product />}>
+          <Route path="/:id" element={<Product />} />
+        </Route>
+        <Route />
+        <Route path="/loginSignup" element={<LoginSignup />} />
+      </Routes>
     </>
   );
 }
---
+++ src/Pages/index.js
@@ -0,0 +1,7 @@
+import { Cart } from "./Cart";
+import { LoginSignup } from "./LoginSignup";
+import { Product } from "./Product";
+import { Shop } from "./Shop";
+import { ShopCategory } from "./ShopCategory";
+
+export { Cart, LoginSignup, Product, Shop, ShopCategory };
