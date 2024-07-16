ef9c00111504f2e840b888afa45c14cc7ca00cc5
twana koye <twana.00569020@gmail.com>
2024-07-16 14:08:22 +0300

    --

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,15 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/men" element={<ShopCategory state="men" />} />
-        <Route path="/women" element={<ShopCategory state="women" />} />
-        <Route path="/kids" element={<ShopCategory state="kids" />} />
+        <Route path="/men" element={<ShopCategory category="men" />} />
+        <Route path="/women" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
         <Route path="/product" element={<Product />}>
-          <Route path="/:id" element={<Product />} />
+          <Route path=":productId" element={<Product />} />
         </Route>
         <Route />
-        <Route path="/loginSignup" element={<LoginSignup />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
       </Routes>
     </>
   );
