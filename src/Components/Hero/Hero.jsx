dd3460297a80ca46b5bc05a6bd6d4f54c2e51ba0
twana koye <twana.00569020@gmail.com>
2024-07-19 22:51:22 +0300

    hero style

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,16 +1,25 @@
 import { handIcon, arrow, heroImage } from "../../assets";
+import "./Hero.css";
 export function Hero() {
   return (
-    <div className="flex justify-between">
-      <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
-        <div>
-          <div>
-            <p>new</p>
-            <img src={handIcon} alt="hand emoji" />
+    <div className="flex justify-between h-full bg-hero ">
+      <div
+        className="hero-left flex flex-col p-40 gap-3
+      font-poppins"
+      >
+        <h2
+          className=" text-xl
+      font-[600]  text-[#302e2e]"
+        >
+          NEW ARRIVALS ONLY
+        </h2>
+        <div className="flex flex-col -space-y-8">
+          <div className="flex items-center gap-2">
+            <p className="text-[#090909] font-[600] text-[80px]">new</p>
+            <img src={handIcon} alt="hand emoji" width={100} />
           </div>
-          <p>collections</p>
-          <p>for everyone</p>
+          <p className="text-[#090909] font-[600] text-[80px]">collections</p>
+          <p className="text-[#090909] font-[600] text-[80px]">for everyone</p>
         </div>
         <div>
           <div className="hero-latest-btn">
