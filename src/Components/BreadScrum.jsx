b97c9734aa95de0271542312a6a52e65f20390fd
twana koye <twana.00569020@gmail.com>
2024-08-03 19:12:55 +0300

    __

--- src/Components/BreadScrum.jsx
+++ src/Components/BreadScrum.jsx
@@ -1,3 +1,14 @@
+import { arrow } from "../assets";
+
 export function BreadScrum(props) {
-  return <div></div>;
+  //disturcturing the props
+  const { product } = props;
+  return (
+    <div className="flex">
+      Home <img src={arrow} alt=">" className="" />
+      Shop <img src={arrow} alt=">" />
+      {product.category} <img src={arrow} alt=">" />
+      {product.name}
+    </div>
+  );
 }
--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,14 +1,18 @@
+import { Link } from "react-router-dom";
+
 export function Item(props) {
   return (
     <div className="w-[300px] font-poppins overflow-hidden ">
-      <img
-        src={props.image}
-        alt="product"
-        height={368}
-        width={300}
-        className="object-contain hover:scale-105
+      <Link to={`/product/${props.id}`}>
+        <img
+          src={props.image}
+          alt="product"
+          height={368}
+          width={300}
+          className="object-contain hover:scale-105
     transition duration-500"
-      />
+        />
+      </Link>
       <p className="my-1.5 font-palanquin font-medium">{props.name}</p>
       <div className="flex gap-5">
         <p className="text-[#374151] text-[18px] font-[600]">
