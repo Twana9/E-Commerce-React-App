4933a26fdc9f1740e9eb1c44f972f43bccd4ef7e
twana koye <twana.00569020@gmail.com>
2024-08-10 20:31:31 +0300

    responsive dispaly product

--- src/Components/BreadScrum.jsx
+++ src/Components/BreadScrum.jsx
@@ -5,9 +5,10 @@ export function BreadScrum(props) {
   const { product } = props;
   return (
     <div
-      className="flex items-center gap-2 my-[60px] mx-[170px]
+      className="flex items-center gap-2 my-[60px] mx-[130px]
     font-poppins text-base tracking-tight max-container
-    text-capitalize font-semibold text-[#5e5e5e]"
+    text-capitalize font-semibold text-[#5e5e5e]
+    max-xl:mx-[60px] max-xl:my-[40px]"
     >
       Home
       <img src={newArrow} alt=">" className="" />
--- src/Components/ProductDisplay.jsx
+++ src/Components/ProductDisplay.jsx
@@ -4,48 +4,67 @@ export function ProductDisplay(props) {
   const { product } = props;
   return (
     <div
-      className="flex  my-0 mx-[170px] 
-      max-container "
+      className="flex  my-0 mx-[130px] 
+      max-container max-xl:mx-[60px]
+      max-lg:flex-col "
     >
       <div
         className="left flex  
-       gap-[17px]"
+       gap-[17px] max-xl:gap-[10px] max-lg:gap-[15px]"
       >
-        <div className="image-list flex flex-col gap-4">
+        <div
+          className="image-list flex flex-col gap-4
+        max-xl:gap-[10px] max-lg:gap-[15px]"
+        >
           <img
             src={product.image}
             alt="item"
-            className="h-[131px] w-[200px] "
+            className="h-[131px] w-[200px] 
+            max-xl:h-[111px] max-lg:w-[100px] "
           />
           <img
             src={product.image}
             alt="item"
-            className="h-[131px] w-[200px] "
+            className="h-[131px] w-[200px] 
+            max-xl:h-[111px] max-lg:w-[100px] "
           />
           <img
             src={product.image}
             alt="item"
-            className="h-[131px] w-[200px] "
+            className="h-[131px] w-[200px] 
+            max-xl:h-[111px] max-lg:w-[100px] "
           />
           <img
             src={product.image}
             alt="item"
-            className="h-[131px] w-[200px] "
+            className="h-[131px] w-[200px] 
+            max-xl:h-[111px] max-lg:w-[100px] "
           />
         </div>
         <div className="main-img">
           <img
             src={product.image}
             alt="main-item"
-            className="h-[574px] w-[890px]"
+            className="h-[574px] w-[890px]
+            max-xl:h-[473px] max-xl:w-[700px]
+            max-lg:w-[400px] max-lg:h-[488px]"
           />
         </div>
       </div>
-      <div className="right flex flex-col mx-[70px] ">
-        <h1 className="text-[#3d3d3d] text-[40px] font-bold">{product.name}</h1>
+      <div
+        className="right flex flex-col mx-[70px]
+      max-xl:mx-[30px] max-lg:mx-0 max-lg:mt-[20px]"
+      >
+        <h1
+          className="text-[#3d3d3d] text-[40px] font-bold
+        min-w-[418px] max-xl:text-3xl"
+        >
+          {product.name}
+        </h1>
         <div
           className="right-star flex gap-1.5 items-center 
-        text-base text-[#1c1c1c] mt-[13px]"
+        text-base text-[#1c1c1c] mt-[13px] 
+        max-xl:mt-[10px]"
         >
           <img src={starIcon} alt="star" />
           <img src={starIcon} alt="star" />
@@ -54,21 +73,26 @@ export function ProductDisplay(props) {
           <img src={starDullIcon} alt="star_dull" />
           <p>(122)</p>
         </div>
-        <div className="flex gap-3 text-xl font-bold mt-10">
+        <div
+          className="flex gap-3 text-xl font-bold mt-10
+        max-xl:mt-6"
+        >
           <div className="text-slate-gray line-through ">
             {product.old_price}$
           </div>
           <div className="text-coral-red">{product.new_price}$</div>
         </div>
-        <div className="text-base text-slate-gray mt-7">
+        <div
+          className="text-base text-slate-gray mt-7
+        max-xl:mt-3"
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          cumque sit quos harum tempore sapiente explicabo error repudiandae
-          magnam facilis.
+          cumque sit quos harum tempore sapiente explicabo
         </div>
         <div>
           <h1
             className="mt-10 text-lg text-slate-gray font-semibold
-          "
+           max-xl:mt-5"
           >
             Select Size
           </h1>
@@ -124,11 +148,14 @@ export function ProductDisplay(props) {
         >
           ADD TO CART
         </button>
-        <p className="text-lg text-slate-gray mt-10">
+        <p
+          className="text-lg text-slate-gray mt-10 max-xl:mt-4
+        max-lg:mt-8"
+        >
           <span className="text-black font-semibold">Category :</span> Unknown,
           T-Shirt, Crop Top
         </p>
-        <p className="text-lg text-slate-gray mt-2">
+        <p className="text-lg text-slate-gray mt-2 max-xl:mt-1">
           <span className="text-black font-semibold">Tags :</span> Modern,
           Latest
         </p>
