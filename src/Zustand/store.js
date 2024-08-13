176bda08f6c59b733131cd7aa12aeaae11331f32
twana koye <twana.00569020@gmail.com>
2024-08-13 20:12:07 +0300

    zustand

--- src/Zustand/store.js
+++ src/Zustand/store.js
@@ -3,3 +3,15 @@ import all_product from "../assets/all_product";
 export const useProduct = create((set) => ({
   products: all_product,
 }));
+
+export const useCart = create((set) => ({
+  cart: [],
+  addToCart: (itemId) =>
+    set((state) => ({
+      cart: [...state.cart, itemId],
+    })),
+  removeFromCart: (itemId) =>
+    set((state) => ({
+      cart: state.cart.filter((item) => item.id !== itemId),
+    })),
+}));
