b902c0785e51367394b6f2daf00e030d0ea14df1
twana koye <twana.00569020@gmail.com>
2024-08-17 14:45:06 +0300

    state managing

--- src/Zustand/store.js
+++ src/Zustand/store.js
@@ -7,19 +7,31 @@ export const useProduct = create((set) => ({
 export const useCart = create((set) => ({
   cart: getDefaultCart(),
   addToCart: (itemId) => {
-    set((state) => {
+    set((state) => ({
       //[itemId] is used to access the value of itemId for example [itemId] = 1
       // so its {...state.cart, 1 : state.cart[1] + 1}
-      const updatedCart = { ...state.cart, [itemId]: state.cart[itemId] + 1 };
-      console.log(updatedCart); // Log the updated cart here
-      return { cart: updatedCart };
-    });
+      cart: { ...state.cart, [itemId]: state.cart[itemId] + 1 },
+    }));
   },
   removeFromCart: (itemId) =>
     set((state) => ({
       cart: { ...state.cart, [itemId]: state.cart[itemId] - 1 },
     })),
+  getTotalCartAmount: () => {
+    let totalAmount = 0;
+    for (let item in cart) {
+      if (cart[item] > 0) {
+        let itemInfo = all_product.find(
+          (product) => product.id === Number(item)
+        );
+        totalAmount += itemInfo.new_price * cart[item];
+      }
+    }
+    return totalAmount;
+  },
 }));
+
+////
 function getDefaultCart() {
   let cart = {};
   for (let index = 0; index < all_product.length + 1; index++) {
